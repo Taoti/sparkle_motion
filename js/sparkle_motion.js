522129086a25ea16f28f95721294c2c859930a56
Nate Mow <natemow@gmail.com>
2017-09-21 16:52:13 -0400

    Nesting the docroot.

---
+++ js/sparkle_motion.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+
+	Drupal.behaviors.sparkle_motion = {
+		attach: function(context, settings) {
+			//this.functionName(context, settings);
+		},
+
+
+		// functionName: function(context, settings) {
+		//
+		// },
+
+
+	} // Drupal.behaviors.sparkle_motion
+
+
+})(jQuery);
